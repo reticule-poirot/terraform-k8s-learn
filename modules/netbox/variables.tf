da1521a758cef2af95ed9beadff81fe7b11327f8
reticule-poirot <56135698+reticule-poirot@users.noreply.github.com>
2023-09-23 00:44:43 +0300

    Revert "tls variables descriptions and docs"
    This reverts commit 52fb7122e58a201e9ab155594ba3fa3f47886a34.

--- modules/netbox/variables.tf
+++ modules/netbox/variables.tf
@@ -12,13 +12,13 @@ variable "fqdn" {
 
 variable "tls_cert" {
   type        = string
-  description = "Netbox tls certificate base64 encoded"
+  description = "Netbox tls certificate"
   sensitive   = true
 }
 
 variable "tls_key" {
   type        = string
-  description = "Netbox tls certificate key base64 encoded"
+  description = "Netbox tls certificate key"
   sensitive   = true
 }
 
