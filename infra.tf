b165ae7ad3ed49306a6f12ccf84b2f04150addf4
reticule-poirot <56135698+reticule-poirot@users.noreply.github.com>
2023-09-27 22:39:55 +0300

    Base infra remote state

--- infra.tf
+++ infra.tf
@@ -14,3 +14,10 @@ provider "kubernetes" {
   client_key             = base64decode(var.client_key)
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
+
+terraform {
+  backend "pg" {
+    // do not do this
+    conn_str = "postgres://terraform:terraform@localhost/terraform?sslmode=disable"
+  }
+}
